6d58d680933396dcaff060a583cd8d6aa5782d95
purushothaman-003 <003purushoth@gmail.com>
2024-08-06 23:29:02 -0700

    project updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
+import { FaTrashAlt } from "react-icons/fa";
 
-function App() {
+function ShoppingList() {
+  const [myList, setMyList] = useState(["Tomato", "Potato", "Orange", "Lemon", "Fruits"]);
+  const [item, setItem] = useState("");
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
+  const addItem = () => {
+    if (item.trim() !== "") {
+      setMyList([...myList, item]);
+      setItem("");
+    }
+  };
+
+  const deleteItem = (index) => {
+    setMyList(myList.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        value={item}
+        onChange={handleChange}
+        placeholder="Enter your item"
+      />
+      <button onClick={addItem}>Add</button>
+      <ul>
+        {myList.map((item, index) => (
+          <li key={index} className="list-item">
+            {item}
+            <FaTrashAlt
+              className="delete-icon"
+              onClick={() => deleteItem(index)}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App;
+export default ShoppingList;
